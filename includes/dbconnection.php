b3d287f12b165f95f6476c42841b688b1308dfdf
nicoleboac <nicx0716@gmail.com>
2015-05-03 22:18:42 +0800

    dbase connection in php

---
+++ includes/dbconnection.php
@@ -0,0 +1,12 @@
+<?php
+$username = "root";
+$password = "";
+dbname = "enrollment";
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+
+?>
