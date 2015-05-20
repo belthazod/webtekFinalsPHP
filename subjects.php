3ee28d43d9f1d9fb0cecd3ef5cf4438e8f8231d0
rej017 <regz_jane01@yahoo.com>
2015-05-20 11:34:09 +0800

    drag and drop working except first row :(

--- subjects.php
+++ subjects.php
@@ -150,164 +150,93 @@
           <div class="clear"></div>
         </div>
       </div>
-
-      
-
     </div>
       
     </div>
    
-      
-         
-
-      <div class="col-md-6">
-        <div class="panel panel-primary">
-          <div class="panel-heading">
-            <h3 class="panel-title"> Available Subjects</h3>
-            
-          </div>
-         <!--  <div class="panel-body">
-            <input type="text" class="form-control" id="dev-table-filter" data-action="filter" data-filters="#t_draggable1" placeholder="Filter Available Subjects" />
-          </div> -->
-          
-    <table class="tables_ui table table-hover" id="t_draggable1" data-toggle="bootgrid" data-ajax="true" data-url="subjectserver.php">
-    <thead>
-            <tr>
+    <div class="col-md-6">
+      <div class="panel panel-primary">
+        <div class="panel-heading">
+          <h3 class="panel-title"> Available Subjects</h3>
+        </div>
+        
+        <table class="tables_ui table table-hover" id="t_draggable1" data-toggle="bootgrid" data-ajax="true" data-url="subjectserver.php">
+          <tbody class="t_sortable">
+            <thead>
+              <tr>
                 <th data-column-id="classcode" data-identifier="true">Classcode</th>
                 <th data-column-id="courseno">Course No.</th>
-        <th data-column-id="description">Descriptive Title</th>
-        <th data-column-id="time">Time</th>
-        <th data-column-id="days">Days</th>
-        <th data-column-id="room">Room</th>
-        <th data-column-id="units">Units</th>
-            </tr>
-    </thead>  
-</table>
+                <th data-column-id="description">Descriptive Title</th>
+                <th data-column-id="time">Time</th>
+                <th data-column-id="days">Days</th>
+                <th data-column-id="room">Room</th>
+                <th data-column-id="units">Units</th>
+              </tr>
+            </thead>  
+          </tbody>
+        </table>
 
-    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-    <script src="js/jquery-1.11.1.min.js"></script>
-    <!-- Include all compiled plugins (below), or include individual files as needed -->
-    <script src="js/bootstrap.min.js"></script>
-    <!-- Include bootgrid plugin (below), -->
-    <script src="js/jquery.bootgrid.min.js"></script>
-
-    <script language="javascript">
-
-    //load gird on page\e load...
-    $("#grid-data").bootgrid(
-    {
-      caseSensitive:false
-    });
-    </script>
-
-
-<!-- Pagination 
-        <div class="row text-center">
-            <div class="col-lg-12">
-                <ul class="pagination">
-                    <li>
-                        <a href="#">&laquo;</a>
-                    </li>
-                    <li class="active">
-                        <a href="#">1</a>
-                    </li>
-                    <li>
-                        <a href="#">2</a>
-                    </li>
-                    <li>
-                        <a href="#">3</a>
-                    </li>
-                    <li>
-                        <a href="#">4</a>
-                    </li>
-                    <li>
-                        <a href="#">5</a>
-                    </li>
-                    <li>
-                        <a href="#">&raquo;</a>
-                    </li>
-                </ul>
-            </div>
-        </div> --> 
-
-</div>
-
-
-        </div>
-
-
-  
-
-
-
-
-
-  <div class="col-md-6">
-        <div class="panel panel-success">
-          <div class="panel-heading">
-            <h3 class="panel-title"> Added Subjects</h3>
-            
-          </div>
-          <div class="panel-body">
-            <input type="text" class="form-control" id="dev-table-filter" data-action="filter" data-filters="#t_draggable2" placeholder="Filter Subjects" />
-          </div>
-          
-    <table class="tables_ui table table-hover" id="t_draggable2" onmouseover="displayUnits(event)">
-      <tbody class="t_sortable">
-
-        <tr>
-          <th>Class Code</th>
-          <th>Course Number</th>
-          <th>Descriptive Title</th>
-          <th>Time</th>
-          <th>Days</th>
-      <th>Room</th>
-      <th>Units</th>
-
-        </tr>
-
-
-      </tbody>
-    </table>
-
-
-</div>
-     <p id="noOfUnits">Total Number of Units: <span id="units"></span></p>
-
-
-   <div class="col-md-10">
-         
-        </div>
-          <form id="eform" onsubmit="return isValidForm()" action="payments.php" method="post">
-          <div class="col-md-2">
-         <button type="submit" class="btn btn-primary" onclick="validateUnits()">Pre-Enroll</button>
-        </div>
-
-
-        </div>
-     
-
+        <script src="jq/jquery-2.1.4.min.js"></script>
+        <script src="jq/jquery-ui.min.js"></script>
+        <script src="js/jquery.bootgrid.min.js"></script>
+        <script language="javascript">
+          //load gird on page\e load...
+          $("#grid-data").bootgrid(
+          {
+            caseSensitive:false
+          });
+        </script>
       </div>
     </div>
-  
-  
 
+    <div class="col-md-6">
+      <div class="panel panel-success">
 
+        <div class="panel-heading">
+          <h3 class="panel-title"> Added Subjects</h3>
+        </div>
 
+        <div class="panel-body">
+          <input type="text" class="form-control" id="dev-table-filter" data-action="filter" data-filters="#t_draggable2" placeholder="Filter Subjects" />
+        </div>
+      
+        <table class="tables_ui table table-hover" id="t_draggable2" onmouseover="displayUnits(event)">
+          <tbody class="t_sortable">
+            <tr>
+              <th>Class Code</th>
+              <th>Course Number</th>
+              <th>Descriptive Title</th>
+              <th>Time</th>
+              <th>Days</th>
+              <th>Room</th>
+              <th>Units</th>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    
+      <p id="noOfUnits">Total Number of Units: <span id="units"></span></p>
+      <div class="col-md-10">
+        </div>
+          <form id="eform" onsubmit="return isValidForm()" action="payments.php" method="post">
+        <div class="col-md-2">
+          <button type="submit" class="btn btn-primary" onclick="validateUnits()">Pre-Enroll</button>
+          </div>
+    </div>
+    </div>
+    </div>
 
-        <!-- Footer -->
-        <footer>
-            <div class="row">
-                <div class="col-lg-12">
-                    <p> WEBTEK 2015</p>
-                </div>
+    <!-- Footer -->
+    <footer>
+        <div class="row">
+            <div class="col-lg-12">
+                <p> WEBTEK 2015</p>
             </div>
-        </footer>
+        </div>
+    </footer>
 
     </div>
     <!-- /.container -->
-
-    
 
     <!-- Bootstrap Core JavaScript -->
     <script src="js/bootstrap.min.js"></script>
