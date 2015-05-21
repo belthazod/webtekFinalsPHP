135c869b2d3dcdc17a4aa7411d7d76cb83ab7a2f
rej017 <regz_jane01@yahoo.com>
2015-05-21 20:04:13 +0800

    edited subjectserver.php

--- subjectserver.php
+++ subjectserver.php
@@ -1,74 +1,76 @@
 <?php
-$host = "localhost";
-$user = "root";
-$pass = "";
-$dbname = "enrollment";
+// Regula Jane S. - - - - -
 
-try {
-   $conn = new PDO("mysql:host=$host;dbname=$dbname", $user, $pass); //MYSQL database
-//$conn = new PDO("sqlite:db/movies.db");  // SQLite Database
- $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  $host = "localhost";
+  $user = "root";
+  $pass = "";
+  $dbname = "enrollment";
 
-$where =" 1=1 ";
- $order_by="courseno";
- $rows=25;
- $current=1;
- $limit_l=($current * $rows) - ($rows);
- $limit_h=$limit_l + $rows  ;
+  try {
+    $conn = new PDO("mysql:host=$host;dbname=$dbname", $user, $pass);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    $where =" 1=1 ";
+    $order_by="courseno";
+    $rows=25;
+    $current=1;
+    $limit_l=($current * $rows) - ($rows);
+    $limit_h=$limit_l + $rows  ;
 
 
-//Handles Sort querystring sent from Bootgrid
-if (isset($_REQUEST['sort']) && is_array($_REQUEST['sort']) )
-  {
-    $order_by="";
-    foreach($_REQUEST['sort'] as $key=> $value)
-    $order_by.=" $key $value";
+    //Sort
+    if (isset($_REQUEST['sort']) && is_array($_REQUEST['sort']) )
+      {
+        $order_by="";
+        foreach($_REQUEST['sort'] as $key=> $value)
+        $order_by.=" $key $value";
+      }
+
+    //Search 
+    if (isset($_REQUEST['searchPhrase']) )
+      {
+        $search=trim($_REQUEST['searchPhrase']);
+        $where.= " AND ( courseno LIKE '".$search."%' OR  year LIKE '".$search."%' OR  description LIKE '".$search."%' ) "; 
+      }
+
+    //Row Count
+    if (isset($_REQUEST['rowCount']) )  
+      $rows=$_REQUEST['rowCount'];
+
+     //Low and High Limits
+      if (isset($_REQUEST['current']) )  
+      {
+       $current=$_REQUEST['current'];
+      $limit_l=($current * $rows) - ($rows);
+      $limit_h=$rows ;
+       }
+
+    if ($rows==-1)
+      $limit="";  //no limit
+    else   
+      $limit=" LIMIT $limit_l,$limit_h ";
+       
+    //Query (Warning: Prone to SQL injection.)
+    $sql="SELECT classcode, courseno, description, time, days,room, units from class natural join course WHERE $where ORDER BY $order_by $limit";
+
+    $stmt=$conn->prepare($sql);
+    $stmt->execute();
+    $results_array=$stmt->fetchAll(PDO::FETCH_ASSOC);
+
+    $json=json_encode( $results_array );
+
+    $nRows=$conn->query("SELECT count(*) from class natural join course WHERE $where")->fetchColumn();
+
+    header('Content-Type: application/json');
+
+    //Bootgrid Library
+    if (isset($_REQUEST['rowCount']) )
+      echo "{ \"current\":  $current, \"rowCount\":$rows,  \"rows\": ".$json.", \"total\": $nRows }";
+    else
+      echo $json;
+    exit;
   }
-
-//Handles search  querystring sent from Bootgrid 
-if (isset($_REQUEST['searchPhrase']) )
-  {
-    $search=trim($_REQUEST['searchPhrase']);
-    $where.= " AND ( courseno LIKE '".$search."%' OR  year LIKE '".$search."%' OR  description LIKE '".$search."%' ) "; 
+    catch(PDOException $e) {
+      echo 'SQL PDO ERROR: ' . $e->getMessage();
   }
-
-//Handles determines where in the paging count this result set falls in
-if (isset($_REQUEST['rowCount']) )  
-  $rows=$_REQUEST['rowCount'];
-
- //calculate the low and high limits for the SQL LIMIT x,y clause
-  if (isset($_REQUEST['current']) )  
-  {
-   $current=$_REQUEST['current'];
-  $limit_l=($current * $rows) - ($rows);
-  $limit_h=$rows ;
-   }
-
-if ($rows==-1)
-$limit="";  //no limit
-else   
-$limit=" LIMIT $limit_l,$limit_h  ";
-   
-//NOTE: No security here please beef this up using a prepared statement - as is this is prone to SQL injection.
-$sql="SELECT classcode, courseno, description, time, days,room, units from class natural join course WHERE $where ORDER BY $order_by $limit";
-
-$stmt=$conn->prepare($sql);
-$stmt->execute();
-$results_array=$stmt->fetchAll(PDO::FETCH_ASSOC);
-
-$json=json_encode( $results_array );
-
-$nRows=$conn->query("SELECT count(*) from class natural join course WHERE $where")->fetchColumn();   /* specific search then how many match */
-
-header('Content-Type: application/json'); //tell the broswer JSON is coming
-
-if (isset($_REQUEST['rowCount']) )  //Means we're using bootgrid library
-echo "{ \"current\":  $current, \"rowCount\":$rows,  \"rows\": ".$json.", \"total\": $nRows }";
-else
-echo $json;  //Just plain vanillat JSON output 
-exit;
-}
-catch(PDOException $e) {
-    echo 'SQL PDO ERROR: ' . $e->getMessage();
-}
 ?>
