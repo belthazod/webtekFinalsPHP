925f6337db75794c5890c1f8c2cebfd852340ca7
nicoleboac <nicx0716@gmail.com>
2015-05-25 09:01:18 +0800

    fixed viewing of schedule

--- schedule.php
+++ schedule.php
@@ -38,9 +38,7 @@
       </thead>
       <tbody>
       <?php
-      $sql = "SELECT count(classcode) as count, classcode,courseno, description, days, class.time, room
-from student natural join enrollment natural join enrollmentdetails join class using(classcode)
-    join course using (courseno) where idno = ".$_SESSION["idno"];
+      $sql = "SELECT  classcode, courseno, description, class.time, days,room, units FROM enrollmentdetails join class using (classcode) join course using (courseno) join enrollment using (enrolid) where idno =".$_SESSION["idno"];
  
           $result = $conn->query($sql);
           if ($result->num_rows > 0) {
@@ -61,72 +59,7 @@ from student natural join enrollment natural join enrollmentdetails join class u
               }
               ?>
 
-        <!--<tr>
-          <td>9570</td>
-          <td>IT 324L</td>
-          <td>Web Systems and Technologies</td>
-          <td>1:00-2:30 WS</td>
-      <td>S326</td>
-      <td>1</td>
-              <a href="user.html"><i class="icon-pencil"></i></a>
-              <a href="#myModal" role="button" data-toggle="modal"><i class="icon-remove"></i></a>
-          </td>
-        </tr>
-        <tr>
-          <td>9569</td>
-          <td>IT 324</td>
-          <td>Web Systems and Technologies</td>
-          <td>10:30-11:30</td>
-      <td>S423</td>
-      <td>2</td>
-              <a href="user.html"><i class="icon-pencil"></i></a>
-              <a href="#myModal" role="button" data-toggle="modal"><i class="icon-remove"></i></a>
-          </td>
-        </tr>
-        <tr>
-          <td>9571</td>
-          <td>IT 323L</td>
-          <td>Integrative Programming</td>
-          <td>1:00-2:30 MTH</td>
-      <td>S326</td>
-      <td>1</td>
-              <a href="user.html"><i class="icon-pencil"></i></a>
-              <a href="#myModal" role="button" data-toggle="modal"><i class="icon-remove"></i></a>
-          </td>
-        </tr>
-        <tr>
-          <td>9572</td>
-          <td>IT 323</td>
-          <td>Integrative Programming</td>
-          <td>1:00-2:00 TF</td>
-      <td>S423</td>
-      <td>2</td>
-              <a href="user.html"><i class="icon-pencil"></i></a>
-              <a href="#myModal" role="button" data-toggle="modal"><i class="icon-remove"></i></a>
-          </td>
-        </tr>
-        <tr>
-          <td>9558</td>
-          <td>IT 423</td>
-          <td>Information Systems Planning </td>
-          <td>9:30-10:30</td>
-      <td>S425</td>
-      <td>3</td>
-              <a href="user.html"><i class="icon-pencil"></i></a>
-              <a href="#myModal" role="button" data-toggle="modal"><i class="icon-remove"></i></a>
-          </td>
-        </tr>
-        <tr>
-          <td>9560</td>
-          <td>IT 312L</td>
-          <td>Programming Applications</td>
-          <td>10:30-12:00 WS</td>
-      <td>S326</td>
-      <td>1</td>
-              <a href="user.html"><i class="icon-pencil"></i></a>
-              <a href="#myModal" role="button" data-toggle="modal"><i class="icon-remove"></i></a>
-          </td>
-        </tr>-->
+       
       </tbody>
     </table>
 </div>
